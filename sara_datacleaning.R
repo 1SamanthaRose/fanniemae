22da5e1337fbbb2fca4808ea689f5e1227790738
spetrosian11 <spetrosi@GMU.edu>
2024-10-12 14:20:40 -0400

    cleaned and fixed grad_rates and enrollment

--- sara_datacleaning.R
+++ sara_datacleaning.R
@@ -1,2 +1,21 @@
 
 #Fix errors with grad_rates and enrollment_grade
+
+install.packages("tidyr")
+library(tidyr)
+
+grad_rates_clean <- grad_rates_demographics |> 
+separate(
+  col = `On-time Graduation Rate`,
+  into = c("year", "level", "division", "subgroup", "graduation_count", "on_time_grad_rate", "completion_count", "completion_rate"),  
+           sep = ",",
+           convert = TRUE
+  )
+
+enrollment_clean <- enrollment_grade |> 
+  separate(
+    col = `Enrollment`,
+    into = c("year", "level", "division", "grade", "count"),  
+    sep = ",",
+    convert = TRUE
+  )
