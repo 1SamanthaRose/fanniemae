9c082d9e1de88ec670adc2ad78baa7001b078993
Samantha Lawrence <slawre2@gmu.edu>
2024-10-12 16:25:39 -0400

    cleaned file

--- samantha_datacleaning.R
+++ samantha_datacleaning.R
@@ -1,6 +1,5 @@
 
 
-
 #READING IN THE DATA
 labor_unemployment = read.csv("Labor_unemployment.csv", header = TRUE)
 median_income = read.csv("median_income.csv", header = TRUE)
@@ -9,7 +8,7 @@ household_size = read_excel("household_size.xlsx")
 owners_renters = read.csv("owners_renters.csv", header = TRUE)
 pop_sex_age = read.csv("population_sex_age.csv", header = TRUE)
 home_sale_prices = read_excel("sales_prices.xlsx")
-traffic = read_excel("traffic.xls")
+
     
     #issues reading in data
           #more columns than column names
