b2ac22e91b4d8038d106f4f11f70f6fcb61129a9
Samantha Lawrence <slawre2@gmu.edu>
2024-10-12 17:10:15 -0400

    fixed a line in my datacleaning file

--- samantha_datacleaning.R
+++ samantha_datacleaning.R
@@ -6,7 +6,7 @@ median_income = read.csv("median_income.csv", header = TRUE)
 home_sales = read_excel("home_sales.xlsx")
 household_size = read_excel("household_size.xlsx")
 owners_renters = read.csv("owners_renters.csv", header = TRUE)
-pop_sex_age = read.csv("population_sex_age.csv", header = TRUE)
+pop_sex = read.csv("population_sex.csv", header = TRUE)
 home_sale_prices = read_excel("sales_prices.xlsx")
 
 #fixing rows
