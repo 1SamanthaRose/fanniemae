5da3797d8cef1b7230734e9d7e5441217c7d22ff
Samantha Lawrence <slawre2@gmu.edu>
2024-10-12 16:34:56 -0400

    fixing rows

--- samantha_datacleaning.R
+++ samantha_datacleaning.R
@@ -8,3 +8,10 @@ household_size = read_excel("household_size.xlsx")
 owners_renters = read.csv("owners_renters.csv", header = TRUE)
 pop_sex_age = read.csv("population_sex_age.csv", header = TRUE)
 home_sale_prices = read_excel("sales_prices.xlsx")
+
+#fixing rows
+
+home_sales = select(home_sales, -c(3:14))
+
+home_sale_prices = select(home_sale_prices, -c(3:105))
+
