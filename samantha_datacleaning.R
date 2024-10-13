ec5cffbcc639ed8b09af16ba13eb0dc3939cfaa6
Samantha Lawrence <slawre2@gmu.edu>
2024-10-13 10:35:47 -0400

    finishing committing

--- samantha_datacleaning.R
+++ samantha_datacleaning.R
@@ -16,6 +16,17 @@ home_sale_prices = home_sale_prices %>%
 #merge datasets
 sales_tenancy = merge(home_sale_prices, owners_renters, by = "county")
 
+#create new variable, percentage of home owners and percentage of home renters
+
+sales_tenancy = sales_tenancy %>%
+                  mutate(owner / total.households)
+
+sales_tenancy = sales_tenancy %>%
+      mutate(tenant / total.households)
+
+sales_tenancy = sales_tenancy %>%
+      rename("percent_renting" = "tenant/total.households", 
+             "percent_owners" = "owner/total.households")
 
 
 #visualisations
@@ -25,16 +36,21 @@ library(scales)
 library(ggplot2)
 
 
-ggscatter(sales_tenancy, x = "tenant", y = "price", 
+ggscatter(sales_tenancy, x = "percent_renting", y = "price", 
           add = "reg.line", conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "kendall",
           xlab = "Home Renters", ylab = "Average Sales Price of Home",
-          xlim = c(0,20000))
-            #ffx co is outlier with 126k renters
+          )
 
 ggscatter(sales_tenancy, x = "total.households", y = "price", 
           add = "reg.line", conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "kendall",
           xlab = "Total Households", ylab = "Average Sales Price of Home",
-          xlim = c(0, 60000), scale_y_continuous(labels = comma))
+          xlim = c(0, 50000))
+
+ggscatter(sales_tenancy, x = "percent_owners", y = "price", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "kendall",
+          xlab = "Home Owners", ylab = "Average Sales Price of Home",
+)
 
